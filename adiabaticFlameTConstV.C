1283603994009dde952fbca1c2109901fe953ac5
Chen Huang <ceng.huang@gmail.com>
2018-07-12 07:42:39 +0200

    sensibleEnthalpy

--- adiabaticFlameTConstV.C
+++ adiabaticFlameTConstV.C
@@ -44,8 +44,8 @@ Description
 #include "perfectGas.H"
 #include "thermo.H"
 #include "janafThermo.H"
-#include "absoluteEnthalpy.H"
-//#include "sensibleInternalEnergy.H"
+//#include "absoluteEnthalpy.H"
+#include "sensibleInternalEnergy.H"
 //#include "sensibleEnthalpy.H"
 
 using namespace Foam;
@@ -220,8 +220,8 @@ int main(int argc, char *argv[])
         scalar Tad = products.THa(reactants.Ha(P, T0), P, 1000.0);
         Info<< "Tad = " << Tad << nl << endl;
 */
-        scalar Ha =  reactants.Ha(P, T0);
-        Info<< "Ha = " << Ha << nl << endl;
+        scalar Tad = products.THs(reactants.Ha(P, T0), P, 1000.0);
+        Info<< "Tad = " << Tad << nl << endl;
 
     }
 
